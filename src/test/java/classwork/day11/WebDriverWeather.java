6d5f013ec23c31897d90e7cc61c518c4180455bd
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-04-12 18:21:27 +0300

    finished task WebDriverWeather

--- src/test/java/classwork/day11/WebDriverWeather.java
+++ src/test/java/classwork/day11/WebDriverWeather.java
@@ -1,18 +1,25 @@
 package classwork.day11;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class WebDriverWeather {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
         WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("погода минск");
-        element.findElement(By.cssSelector("div span"));
-        element.click();
 
+//        element.sendKeys("погода минск");
+//        driver.findElement(By.xpath("//ul[@role='listbox']/li[1]")).click();
+
+        element.sendKeys("погода минск", Keys.ENTER);
+        driver.findElement(By.xpath("//div[@aria-label='чацвер']")).click();
+        WebElement el = driver.findElement(By.xpath("//*[contains(@aria-label, '9°Celsius чацвер 12:00')]"));
+        String str = el.getText();
+        System.out.println(str + "° в 12:00");
     }
 }
