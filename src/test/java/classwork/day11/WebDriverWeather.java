797882e035c0da3a4d4f95befe9d6ee05cb6d393
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-04-13 11:51:34 +0300

    added changes to the code

--- src/test/java/classwork/day11/WebDriverWeather.java
+++ src/test/java/classwork/day11/WebDriverWeather.java
@@ -11,15 +11,17 @@ public class WebDriverWeather {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
         WebElement element = driver.findElement(By.name("q"));
 
 //        element.sendKeys("погода минск");
 //        driver.findElement(By.xpath("//ul[@role='listbox']/li[1]")).click();
 
         element.sendKeys("погода минск", Keys.ENTER);
-        driver.findElement(By.xpath("//div[@aria-label='чацвер']")).click();
-        WebElement el = driver.findElement(By.xpath("//*[contains(@aria-label, '9°Celsius чацвер 12:00')]"));
-        String str = el.getText();
-        System.out.println(str + "° в 12:00");
+        driver.findElement(By.xpath("//div[@aria-label='пятніца']")).click();
+        WebElement el = driver.findElement(By.xpath("//*[contains(@aria-label, '11°Celsius пятніца 12:00')]"));
+        String str = el.getAttribute("aria-label");
+        String[] array = str.split("Celsius");
+        System.out.println(array[0] + " завтра в 12:00");
     }
 }
