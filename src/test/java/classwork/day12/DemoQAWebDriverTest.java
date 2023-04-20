92ff36539e4a02c4fa7319e1b64f7242c1c94bf0
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-04-20 15:40:24 +0300

    finished the task 4

---
+++ src/test/java/classwork/day12/DemoQAWebDriverTest.java
@@ -0,0 +1,43 @@
+package classwork.day12;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class DemoQAWebDriverTest {
+    WebDriver driver = new ChromeDriver();
+
+    @Before
+    public void dataCreation() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(20));
+        driver.get("https://demoqa.com/select-menu");
+    }
+
+    @Test
+    public void selectOptions() {
+        driver.findElement(By.xpath("//div[@id='withOptGroup']")).click();
+        driver.findElement(By.xpath("//div[text()='Group 1, option 2']")).click();
+        driver.findElement(By.xpath("//div[@id='selectOne']")).click();
+        driver.findElement(By.xpath("//div[text()='Mrs.']")).click();
+        Select selectColor = new Select(driver.findElement(By.id("oldSelectMenu")));
+        selectColor.selectByValue("3");
+        driver.findElement(By.xpath("//div[text()='Select...']/..")).click();
+        driver.findElement(By.xpath("//div[text()='Blue']")).click();
+        Select selectCars = new Select(driver.findElement(By.id("cars")));
+        selectCars.selectByIndex(2);
+    }
+
+    @After
+    public void driverClose() {
+        driver.quit();
+    }
+}
