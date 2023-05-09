d08ae051a25d501bc15cc6e90e1641746b840649
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-05-09 13:05:03 +0300

    added logger

--- src/test/java/pages/google/GoogleMainPage.java
+++ src/test/java/pages/google/GoogleMainPage.java
@@ -1,12 +1,15 @@
 package pages.google;
 
+import classwork.day11.WebDriverWeather;
 import driver.Driver;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
 public class GoogleMainPage {
+    private static final Logger LOGGER = Logger.getLogger(WebDriverWeather.class.getName());
     private final By SEARCH_AREA = By.xpath("//textarea[@role='combobox']");
     private final By HEADLINES = By.xpath("//h3");
     private final By HEADLINES_WITH_TUTORIAL = By.xpath("//h3[contains(text(), 'tutorial') or contains(text(), 'Tutorial') or contains(text(), 'TUTORIAL')]");
