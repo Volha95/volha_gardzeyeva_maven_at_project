cb3ac90565ffde24e082922c14e0b3c010a22ef5
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-05-09 13:02:54 +0300

    added logger

---
+++ src/test/java/steps/BookingCucumberTests.java
@@ -0,0 +1,75 @@
+package steps;
+
+import classwork.day11.WebDriverWeather;
+import driver.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import pages.booking.BookingMainPage;
+import pages.booking.FoundHotelsPage;
+
+public class BookingCucumberTests {
+    private static final Logger LOGGER = Logger.getLogger(WebDriverWeather.class.getName());
+    BookingMainPage bookingMainPage = new BookingMainPage();
+    FoundHotelsPage foundHotelsPage = new FoundHotelsPage();
+    @Before
+    public void getDriver() {
+        Driver.getWebDriver();
+    }
+    @After
+    public void closeDriver() {
+        Driver.destroy();
+    }
+
+    @Given("I open booking site")
+    public void openSite() {
+        bookingMainPage.siteLoad();
+        bookingMainPage.dismissInfo();
+    }
+
+    @When("I input the city")
+    public void iInputThe() {
+        bookingMainPage.findHotel("Berlin");
+    }
+
+    @And("Input dates")
+    public void inputDates() {
+        bookingMainPage.inputFirstDate(10, "May", 2023);
+        bookingMainPage.inputSecondDate(18, "May", 2023);
+    }
+
+    @And("Input adults and rooms")
+    public void inputAdultsAndRooms() {
+        bookingMainPage.adultsInput();
+        bookingMainPage.adultsInput();
+        bookingMainPage.roomsInput();
+    }
+
+    @And("Click on button search")
+    public void clickOnButtonSearch() {
+        bookingMainPage.searchButtonClick();
+        foundHotelsPage.spinnerWait();
+    }
+
+    @And("Sort the hotels")
+    public void sortTheHotels() {
+        foundHotelsPage.sortDropdownClick();
+        foundHotelsPage.sortFromCheapest();
+    }
+
+    @Then("Check the cost of the night")
+    public void checkTheCostOfTheNight() {
+        String priceHotel = foundHotelsPage.getFirstHotelPrice().getText();
+        String priceHotelNumberOnly = priceHotel.replaceAll("[^0-9]", "");
+        String priceNight = foundHotelsPage.getPricePerNight().getText();
+        String priceNightNumberOnly = priceNight.replaceAll("[^0-9]", "");
+        double hotelPrice = Double.parseDouble(priceHotelNumberOnly);
+        double nightPrice = Double.parseDouble(priceNightNumberOnly);
+        Assert.assertTrue("the cost of a night in a hotel is less than the minimum", hotelPrice / 7 >= nightPrice);
+    }
+}
