914f9826dd1c5b5880a12307bdcc401df83c58ba
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-05-09 11:47:41 +0300

    added logger

--- src/test/java/classwork/day14/DemoSteps.java
+++ src/test/java/classwork/day14/DemoSteps.java
@@ -1,23 +1,26 @@
 package classwork.day14;
 
+import classwork.day11.WebDriverWeather;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 
 public class DemoSteps {
+    private static final Logger LOGGER = Logger.getLogger(WebDriverWeather.class.getName());
     @Given("I open demoG7 site")
     public void iOpenDemoGSite() {
-        System.out.println("I opened the site");
+        LOGGER.info("I opened the site");
     }
     @When("I fill {string} in the form")
     public void iFillInTheForm(String text) {
-        System.out.printf("I printed '%s' the string", text);
+        LOGGER.info(String.format("I printed '%s' the string", text));
     }
 
     @Then("I need to see something")
     public void iNeedToSeeSomething() {
         Assert.assertTrue(true);
-        System.out.println("I finished");
+        LOGGER.info("I finished");
     }
 }
