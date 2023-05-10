857c0ad1f9252b43611f05ecd6b832eb718e59c9
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-05-10 15:50:52 +0300

    added LOGGER.trace

--- src/test/java/pages/google/GoogleMainPage.java
+++ src/test/java/pages/google/GoogleMainPage.java
@@ -16,13 +16,16 @@ public class GoogleMainPage {
     private final By HEADLINES_WITH_TUTORIAL = By.xpath("//h3[contains(text(), 'tutorial') or contains(text(), 'Tutorial') or contains(text(), 'TUTORIAL')]");
 
     public WebElement getGoogleSearch() {
+        LOGGER.trace("Find google search element, SEARCH_AREA locator: //textarea[@role='combobox']");
         return driver.findElement(SEARCH_AREA);
     }
 
     public List<WebElement> getHeadlines() {
+        LOGGER.trace("Find headlines elements, HEADLINES locator: //h3");
         return driver.findElements(HEADLINES);
     }
     public List<WebElement> getTutorialHeadlines() {
+        LOGGER.trace("Find headlines elements with 'tutorial' word, HEADLINES_WITH_TUTORIAL locator: //h3[contains(text(), 'tutorial') or contains(text(), 'Tutorial') or contains(text(), 'TUTORIAL')]");
         return driver.findElements(HEADLINES_WITH_TUTORIAL);
     }
 }
