c3edba4d789b7541ab58782fd7cbd7f4e07b6ffc
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-05-10 15:12:48 +0300

    added LOGGER.trace

--- src/test/java/pages/booking/RegistrationPage.java
+++ src/test/java/pages/booking/RegistrationPage.java
@@ -1,6 +1,5 @@
 package pages.booking;
 
-import classwork.day11.WebDriverWeather;
 import driver.Driver;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
@@ -18,11 +17,14 @@ public class RegistrationPage {
 
     public void registration() {
         driver.findElement(EMAIL_INPUT_FIELD).sendKeys(TrashmailPage.emailCreation());
+        LOGGER.trace("Input email to email field, EMAIL_INPUT_FIELD locator By id: username");
         driver.findElement(CONTINUE).click();
+        LOGGER.trace("Click on button CONTINUE, locator: //div[@class='hidden-password-input-container']/following-sibling::div/button[1]");
         driver.findElement(NEW_PASSWORD_FIELD).sendKeys("Newpassword123");
+        LOGGER.trace("Input password to first password field, NEW_PASSWORD_FIELD locator: //input[@id='new_password']");
         driver.findElement(CONFIRMED_PASSWORD).sendKeys("Newpassword123");
+        LOGGER.trace("Input password to second password field, CONFIRMED_PASSWORD locator: //input[@id='confirmed_password']");
         driver.findElement(CREATE_ACCOUNT_BUTTON).click();
-
-
+        LOGGER.trace("Click on create account button, CREATE_ACCOUNT_BUTTON locator: //form[@class='nw-register']/button");
     }
 }
