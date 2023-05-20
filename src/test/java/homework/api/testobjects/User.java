233bd087ef2edaf68e3453b88d6261579940b088
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-05-20 10:32:48 +0300

    made homework (api)

---
+++ src/test/java/homework/api/testobjects/User.java
@@ -0,0 +1,83 @@
+package homework.api.testobjects;
+
+import java.util.Objects;
+
+public class User {
+    private String id;
+    private String username;
+    private String realname;
+    private String password;
+    private String email;
+
+    public User(String id, String username, String realname, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.realname = realname;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(realname, user.realname) && Objects.equals(password, user.password) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, realname, password, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", realname='" + realname + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
